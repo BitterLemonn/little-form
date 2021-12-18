d07d2b98d78b0a4b8d023c6289de8004892711b9
bitterlemon <873811906@qq.com>
2021-12-18 23:59:16 +0800

    解决冲突

--- my/js/edit_user.js
+++ my/js/edit_user.js
@@ -8,32 +8,19 @@ function getCookie(cname) {
     }
     return "";
 }
-<<<<<<< HEAD
 // 查询用户基本信息
 function getUser() {
-=======
-
-// 查询用户基本信息
-function getUser(){
->>>>>>> b0625fea7dd2921ce92114fcd24b8f370d31fb3c
     $.ajax({
         url: "updaMe.php",
         type: "GET",
         data: {
-<<<<<<< HEAD
             "uname": getCookie("username")
-=======
-            "uname":getCookie("username")
->>>>>>> b0625fea7dd2921ce92114fcd24b8f370d31fb3c
         },
         success: function (data, textStatus, jqXHR) {
             if (data["code"] == 200) {
                 document.getElementById("userName").value = data["data"]["username"];
                 document.getElementById("email").value = data["data"]["email"];
-<<<<<<< HEAD
                 document.getElementById("profile").src = data["data"]["profile"] || '../../images/me.jpg'
-=======
->>>>>>> b0625fea7dd2921ce92114fcd24b8f370d31fb3c
             }
             else {
                 $("#errHint").text("发生不可预计的错误, 请联系网站管理员。错误详情: code: " + data["code"] + ", message: " + data["message"]);
@@ -46,7 +33,6 @@ function getUser(){
     return false;
 }
 
-<<<<<<< HEAD
 function checkPassword() {
     var password = $("#password").val();
     var password1 = $("#password1").val();
@@ -59,20 +45,6 @@ function checkPassword() {
         $("#passwordHint").text('两次密码不一致');
         return false;
     } else if (rule.test(password)) {
-=======
-function checkPassword(){
-    var password = $("#password").val();
-    var password1 = $("#password1").val();
-    var rule = /^.+$/
-    if(password==""||password1==""){
-        $("#passwordHint").text('密码不能为空');
-        return false;
-    }else if(password!==password1){
-        //输入校验结果
-        $("#passwordHint").text('两次密码不一致');
-        return false;
-    }else if(rule.test(password)){
->>>>>>> b0625fea7dd2921ce92114fcd24b8f370d31fb3c
         $("#passwordHint").text('');
         return true;
     }
@@ -80,21 +52,12 @@ function checkPassword(){
 }
 
 function edit() {
-<<<<<<< HEAD
     if (checkPassword()) {
-=======
-    if (checkPassword()){
->>>>>>> b0625fea7dd2921ce92114fcd24b8f370d31fb3c
         var formData = $("#form").serialize();
         $.ajax({
             url: "updaMe.php",
             type: "POST",
             data: formData,
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> b0625fea7dd2921ce92114fcd24b8f370d31fb3c
             success: function (data, textStatus, jqXHR) {
                 if (data["code"] == 200) {
                     alert("用户信息修改成功");
@@ -113,11 +76,7 @@ function edit() {
         })
         return false;
 
-<<<<<<< HEAD
     } else {
-=======
-    }else{
->>>>>>> b0625fea7dd2921ce92114fcd24b8f370d31fb3c
         return false;
     }
 }
